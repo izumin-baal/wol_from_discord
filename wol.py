adca4d3f00f394e47d57b4139356aafd969a4ad8
izumin <69159964+izumin-baal@users.noreply.github.com>
2021-02-22 01:15:20 +0900

    :sparkles:WoL部分一応完成

--- wol.py
+++ wol.py
@@ -2,6 +2,8 @@ import discord
 import sys
 import time
 import pings
+import socket
+import struct
 from configparser import ConfigParser
 from datetime import datetime, timedelta
 
@@ -15,11 +17,11 @@ BOT_TOKEN = XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
 SERVER_ID = 00000000000000000000000
 # 通知させるチャンネルのID
 ALERT_CHANNEL = 00000000000000000000000
-HOST = {
-    "MainPC": ["XX:XX:XX:XX:XX:AA", "XXX.XXX.XXX.1"],
-    "Server1": ["XX:XX:XX:XX:XX:BB", "XXX.XXX.XXX.2"],
-    "Server2": ["XX:XX:XX:XX:XX:CC", "XXX.XXX.XXX.3"]
-}
+HOST = [
+    ["MainPC", "XX:XX:XX:XX:XX:AA", "XXX.XXX.1"],
+    ["Server1", "XX:XX:XX:XX:XX:BB", "XXX.XXX.XXX.2"],
+    ["Server2", "XX:XX:XX:XX:XX:CC", "XXX.XXX.XXX.3"],
+]
 """
 
 # botのトークン
@@ -30,10 +32,12 @@ SERVER_ID = int(config.get('settings', 'SERVER_ID'))
 ALERT_CHANNEL = int(config.get('settings', 'ALERT_CHANNEL'))
 # ホスト一覧
 HOST = [
-    ["MainPC", "XX:XX:XX:XX:XX:AA", "XXX.XXX.XX1"],
+    ["MainPC", "XX:XX:XX:XX:XX:AA", "XXX.XXX.1"],
     ["Server1", "XX:XX:XX:XX:XX:BB", "XXX.XXX.XXX.2"],
-    ["Server2", "XX:XX:XX:XX:XX:CC", "XXX.XXX.XXX.3"],
+    ["vyos", "XX:XX:XX:XX:XX:BB", "XXX.XXX.XXX.3"],
 ]
+# WoLするセグメントのブロードキャストアドレス
+BROADCAST = "XXX.XXX.XXX.XXX"
 
 client = discord.Client()
 
@@ -48,6 +52,18 @@ async def on_message(message):
             msg = msg + str(num + 1) + ": " + value[0] + "\n"
         reply_select = msg + "\n" + "Which? :"
         await message.channel.send(reply_select)
+
+        def send_magic_packet(addr):
+            DEFAULT_PORT = 7
+            with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
+                s.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+                mac = addr.upper().replace("-", "").replace(":", "")
+                buf = b'f' * 12 + (mac * 20).encode()
+                magicp = b''
+                for i in range(0, len(buf), 2):
+                    magicp += struct.pack('B', int(buf[i:i + 2], 16))
+                s.sendto(magicp, (BROADCAST, DEFAULT_PORT))
+
         def select(m):
             if m.content.isdecimal():
                 if int(m.content) <= len(HOST) and int(m.content) > 0:
@@ -66,9 +82,10 @@ async def on_message(message):
             IPaddress = HOST[int(select_result.content)-1][2]
             await message.channel.send("OK\nsend wol(" + MACaddress + ")\nThis device IP address is " + IPaddress)
         if error_flag != 1:
+            send_magic_packet(MACaddress)
             ping_result = 0
             for i in range(6):
-                time.sleep(5)
+                time.sleep(30)
                 await message.channel.send("ping...")
                 ping = pings.Ping()
                 response = ping.ping(IPaddress)
@@ -79,13 +96,8 @@ async def on_message(message):
                 else:
                     await message.channel.send("unreachable...")
             if ping_result == 1:
-                await message.channel.send("Active")
+                await message.channel.send(f"{message.author.mention}\n:green_circle: Active")
             else:
-                await message.channel.send("disabled")
-
-    if message.content == '!dis':
-        sys.exit()
-        
-
+                await message.channel.send(f"{message.author.mention}\n:red_circle: disabled")
 
 client.run(BOT_TOKEN)
