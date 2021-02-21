42527fe2bcd4adf564ecf8d9302d19bd950aadba
izumin <69159964+izumin-baal@users.noreply.github.com>
2021-02-21 22:59:21 +0900

    :construction: WoLするデバイスを選択して応答がくるまでの処理が完了

--- wol.py
+++ wol.py
@@ -1,4 +1,5 @@
 import discord
+import sys
 from configparser import ConfigParser
 from datetime import datetime, timedelta
 
@@ -12,6 +13,11 @@ BOT_TOKEN = XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
 SERVER_ID = 00000000000000000000000
 # 通知させるチャンネルのID
 ALERT_CHANNEL = 00000000000000000000000
+HOST = {
+    "MainPC": ["XX:XX:XX:XX:XX:AA", "XXX.XXX.XXX.1"],
+    "Server1": ["XX:XX:XX:XX:XX:BB", "XXX.XXX.XXX.2"],
+    "Server2": ["XX:XX:XX:XX:XX:CC", "XXX.XXX.XXX.3"]
+}
 """
 
 # botのトークン
@@ -20,6 +26,12 @@ BOT_TOKEN = str(config.get('settings', 'BOT_TOKEN'))
 SERVER_ID = int(config.get('settings', 'SERVER_ID'))
 # 通知させるチャンネルのID
 ALERT_CHANNEL = int(config.get('settings', 'ALERT_CHANNEL'))
+# ホスト一覧
+HOST = [
+    ["MainPC", "XX:XX:XX:XX:XX:AA", "XXX.XXX.XXX.1"],
+    ["Server1", "XX:XX:XX:XX:XX:BB", "XXX.XXX.XXX.2"],
+    ["Server2", "XX:XX:XX:XX:XX:CC", "XXX.XXX.XXX.3"],
+]
 
 client = discord.Client()
 
@@ -27,7 +39,33 @@ client = discord.Client()
 async def on_message(message):
     if message.author.bot:
         return
-    
+    if message.content == '!wol':
+        msg = ""
+        error_flag = 0
+        for num,value in enumerate(HOST):
+            msg = msg + str(num + 1) + ": " + value[0] + "\n"
+        reply_select = msg + "\n" + "Which? :"
+        await message.channel.send(reply_select)
+        def select(m):
+            if m.content.isdecimal():
+                if int(m.content) <= len(HOST) and int(m.content) > 0:
+                    return m.channel == message.channel and m.author != client.user
+                else:
+                    return False
+            else:
+                return False
+        try:
+            select_result = await client.wait_for('message', check=select, timeout=10.0)
+        except:
+            await message.channel.send("Timeout...")
+            error_flag = 1
+        else:
+            await message.channel.send("OK\nsend wol(" + HOST[int(select_result.content)-1][1] + ")\nThis device IP address is " + HOST[int(select_result.content)-1][2])
+
+
+    if message.content == '!dis':
+        sys.exit()
+        
 
 
 client.run(BOT_TOKEN)
